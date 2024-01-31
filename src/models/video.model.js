526d2d3727f5fff1c8161b290093634acb84175b
prab27 <ayushsin722gmail.com>
2024-01-31 23:11:03 +0530

    update

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -53,4 +53,4 @@ const videoSchema = new Schema (
 
 videoSchema.plugin(mongooseAggregatePaginate)
 
-export const Video = mongoose.model("Videos",videoSchema)
+export const Video = mongoose.model("Video",videoSchema)
